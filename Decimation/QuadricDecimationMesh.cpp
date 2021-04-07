5818c0281e397779aa7bb53cff59625f47852706
Julius Halldan <juliushalldan@gmail.com>
2021-04-07 16:38:44 +0200

    Lab 2 started

--- Decimation/QuadricDecimationMesh.cpp
+++ Decimation/QuadricDecimationMesh.cpp
@@ -38,7 +38,45 @@ void QuadricDecimationMesh::computeCollapse(EdgeCollapse* collapse) {
     // Compute collapse->position and collapse->cost here
     // based on the quadrics at the edge endpoints
 
-    std::cerr << "computeCollapse in QuadricDecimationMesh not implemented.\n";
+    glm::vec3 collapsePosition;
+    float collapseCost;
+
+    size_t v1 = e(collapse->halfEdge).vert;
+    size_t v2 = e(e(collapse->halfEdge).pair).vert;
+
+    glm::mat4 Q1 = mQuadrics[v1];
+    glm::mat4 Q2 = mQuadrics[v2];
+
+    //glm::mat4 Q1 = createQuadricForVert(e(collapse->halfEdge).vert);
+    //glm::mat4 Q2 = createQuadricForVert(e(e(collapse->halfEdge).pair).vert);
+
+    glm::mat4 Qbar = Q1 + Q2;
+    glm::mat4 Qhat = Qbar;
+
+    Qhat[0][3] = 0;
+    Qhat[1][3] = 0;
+    Qhat[2][3] = 0;
+    Qhat[3][3] = 1;
+
+    glm::vec4 zero = {0, 0, 0, 1};
+    glm::vec4 vbar;
+    
+
+    if (glm::determinant(Qhat) < 0.000001f && glm::determinant(Qhat) > -0.000001f) {
+        vbar = glm::inverse(Qhat) * zero;
+     
+        collapseCost = glm::dot(vbar,Qbar*vbar);
+        collapsePosition = vbar;
+        
+    } 
+    else {
+        collapsePosition = (v(e(collapse->halfEdge).vert).pos + v(e(e(collapse->halfEdge).next).vert).pos) / 2.0f;
+        collapseCost = glm::distance(collapsePosition, v(e(collapse->halfEdge).vert).pos);
+    }
+
+    collapse->position = collapsePosition;
+    collapse->cost = collapseCost;
+    
 }
 
 /*! After each edge collapse the vertex properties need to be updated */
@@ -56,6 +94,15 @@ glm::mat4 QuadricDecimationMesh::createQuadricForVert(size_t indx) const {
 
     // The quadric for a vertex is the sum of all the quadrics for the adjacent
     // faces Tip: Matrix4x4 has an operator +=
+
+    std::vector faces = FindNeighborFaces(indx);
+    
+    for (int i = 0; i < faces.size(); i++) {
+    
+        Q += createQuadricForFace(faces[i]);
+    
+    }
+
     return Q;
 }
 
@@ -66,7 +113,32 @@ glm::mat4 QuadricDecimationMesh::createQuadricForFace(size_t indx) const {
 
     // Calculate the quadric (outer product of plane parameters) for a face
     // here using the formula from Garland and Heckbert
-    return glm::mat4();
+
+    float a = f(indx).normal.x;
+    float b = f(indx).normal.y;
+    float c = f(indx).normal.z;
+    float d = -dot(v(e(f(indx).edge).vert).pos, f(indx).normal);
+
+    glm::mat4 quadric;
+
+    quadric[0][0] = a * a;
+    quadric[1][0] = a * b;
+    quadric[2][0] = a * c;
+    quadric[3][0] = a * d;
+    quadric[0][1] = a * b;
+    quadric[1][1] = b * b;
+    quadric[2][1] = b * c;
+    quadric[3][1] = b * d;
+    quadric[0][2] = a * c;
+    quadric[1][2] = b * c;
+    quadric[2][2] = c * c;
+    quadric[3][2] = c * d;
+    quadric[0][3] = a * d;
+    quadric[1][3] = b * d;
+    quadric[2][3] = c * d;
+    quadric[3][3] = d * d;
+
+    return quadric;
 }
 
 void QuadricDecimationMesh::Render() {
